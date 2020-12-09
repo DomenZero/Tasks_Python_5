9f3b6b2f2a523bf2fb22851a39a139e6c989b554
Maksim Merkulov <wardomenmax@gmail.com>
2020-12-09 22:04:08 +0300

    Update les_5_task_2.py

--- les_5_task_2.py
+++ les_5_task_2.py
@@ -7,14 +7,9 @@
 from collections import defaultdict
 from collections import deque
 
-dec_num={'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'A':10,'B':11,'C':12,'D':13,'E':14,'F':15}
+dec_num = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12,
+           'D': 13, 'E': 14, 'F': 15}
 
-
-def sum_hex():
-    print()
-
-def multi_hex():
-    print()
 
 def to_dec(num):
     degree = 1
@@ -35,42 +30,39 @@ def to_dec(num):
 
 def to_hex(num):
     deq = deque()
-    deq_ost=deque()
-    # g = deque()
+    deq_ost = deque()
     g = deque(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'])
-    print(g.index(1))
-    if num > 16:
-        div = num % 16
-        div_c = num // 16
-        deq_ost.append(div_c)
-        if div < 16:
-            deq.appendleft(div)
-        else:
-            while div > 16:
-                div = num % 16
-            if div in dec_num:
-                print(dec_num[div])
-            deq.appendleft(div)
 
-    i=0
-    print()
-    for i in deq:
-        print(deq[i])
+    div_c = num // 16
+    div = num % 16
+    if num > 16:
+        while div_c > 0:
+            if div_c < 16 and div < 16:
+                deq.append(g[div])
+                deq.append(g[div_c])
+
+            elif div < 16:
+                deq.append(g[div])
+            elif div_c < 16:
+                deq.append(g[div_c])
+            div = div_c % 16
+            div_c = div_c // 16
+    else:
+        deq.appendleft(g[num])
+    i = 0
+    deq_all = deq_ost + deq
+    deq_all.reverse()
+    ll = list(deq_all)
+    return ll
+
 
 num1 = list(input('Введите первое hex число: '))
 num2 = list(input('Введите второе hex число: '))
 
-d=defaultdict(set)
-num_dec1=to_dec(num1)
-print(num_dec1)
-num_dec2=to_dec(num2)
-print(num_dec2)
-num_dec=num_dec1+num_dec2
-print(num_dec)
-to_hex(num_dec)
-
-print(num1)
-print(num2)
-
-
-
+d = defaultdict(set)
+num_dec1 = to_dec(num1)
+num_dec2 = to_dec(num2)
+sum_dec = num_dec1 + num_dec2
+print(f'Сумма: {to_hex(sum_dec)}')
+multi_dec = num_dec1 * num_dec2
+print(f'Произведение: {to_hex(multi_dec)}')
